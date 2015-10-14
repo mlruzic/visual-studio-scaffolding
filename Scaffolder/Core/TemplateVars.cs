94fc8f38a2a30698703e2191b982df9074448a98
Mladen Ruzic <mladen@shoutem.com>
2015-10-14 11:32:59 +0200

    Updated templates - added <autogenerated> comment

--- Scaffolder/Core/TemplateVars.cs
+++ Scaffolder/Core/TemplateVars.cs
@@ -1,15 +1,24 @@
 ﻿namespace VSPackage.ScaffolderPackage.Core
 {
+    using System;
     using System.Collections.Generic;
+    using System.Reflection;
     
     /// <summary>
     /// Defines variables that are available for use in templates. 
     /// </summary>
     public class TemplateVars : Dictionary<string, object>
     {
+        private const string dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+
         public TemplateVars(string tableName, IEnumerable<TableColumn> tableColumns, string projectItemName):
             base()
         {
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Add("Version", version);
+            Add("DateTimeNow", DateTime.Now.ToString(dateTimeFormat));
+            Add("DateTimeUtcNow", DateTime.UtcNow.ToString(dateTimeFormat));
+
             Add("TableName", tableName);
             Add("TableColumns", tableColumns);
             Add("TableSchema", projectItemName);
@@ -19,7 +28,6 @@
             Add("ServiceIoName", string.Format("{0}Io", tableName));
             Add("ServiceModelName", string.Format("{0}Model", tableName));
             Add("ControllerName", string.Format("{0}Controller", tableName));
-            
         }
     }
 }
